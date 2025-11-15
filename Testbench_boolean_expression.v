059cd2a9725591621ae3a70026235394f3c00903
Deepanshu sharma <deepanshusharma12098@ce.du.ac.in>
2025-11-15 10:27:26 +0530

    Update Testbench_boolean_expression.v

--- Testbench_boolean_expression.v
+++ Testbench_boolean_expression.v
@@ -1,3 +1,29 @@
-module boolean_expr(input A, B, C, D, output Y);
-  assign Y = (A | B) | (A | C);
+`timescale 1ns/1ps
+
+module boolean_expr_tb;
+
+  reg A, B, C, D;
+  wire Y;
+
+  boolean_expr dut (
+    .A(A),
+    .B(B),
+    .C(C),
+    .D(D),
+    .Y(Y)
+  );
+
+  initial begin
+    $dumpfile("wave.vcd");
+    $dumpvars(0, boolean_expr_tb);
+
+    A=0; B=0; C=0; D=0; #10;
+    A=0; B=0; C=1; D=0; #10;
+    A=0; B=1; C=0; D=0; #10;
+    A=1; B=0; C=0; D=0; #10;
+    A=1; B=1; C=1; D=0; #10;
+
+    $finish;
+  end
+
 endmodule
