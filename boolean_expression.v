38f0e540ecc7da631b1726d3218f8c3fe702b1de
Deepanshu sharma <deepanshusharma12098@ce.du.ac.in>
2025-11-15 10:25:23 +0530

    Refactor boolean_expr module to include input D

--- boolean_expression.v
+++ boolean_expression.v
@@ -1,4 +1,3 @@
-`timescale 1ns / 1ps
-module boolean_expr(input A, input B, input C, output Y);
-    assign Y = (A | B) | (A | C);
+module boolean_expr(input A, B, C, D, output Y);
+  assign Y = (A | B) | (A | C);
 endmodule
